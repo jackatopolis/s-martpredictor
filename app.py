022c98a1f578d89a27719a3fd8c9c74170cedb3d
Jack Cohen <jc@enpira.io>
2021-11-24 01:48:36 -0500

    Added model and data html/js files

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from joblib import load
 from pandas import DataFrame
 
 app = Flask(__name__)
-model = load('filename.joblib')
+model = load('./static/data/filename.joblib')
 # scaler = load('scaler.joblib') # loads scaler in, check filename
 
 
