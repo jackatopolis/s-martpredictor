b2439a2b9fa5f300b7df7f2a02b59d4dab75c837
Uchenna Nwagbara <83881279+uchenna23@users.noreply.github.com>
2021-11-23 22:04:54 -0500

    Flask

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from joblib import load
+from pandas import DataFrame
 
 app = Flask(__name__)
 
-
+model= load('filename.joblib')
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -26,6 +28,40 @@ def about():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+@app.route('/predictor', methods= ['POST'])
+def predictor():
+    dataset = request.json
+    try:
+        input = DataFrame(data = {
+            "week of the year":[int(dataset["week of the year"])],
+            "base price": [int(dataset["base price"])],
+            "promotional price": [int(dataset["promotional price"])],
+            "store": [int(dataset["store"])]
+        })
+    except:
+        return jsonify
+    output = model.predict(input)
+    results= {'output':output, 
+            'price':[int(dataset["promotional price"])],
+            'cost':[int(dataset["cost"])]}
+    return jsonify({"quantity":output[0]})
+
+    
+
+    return render_template('predictor.html')
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+@app.route('/base')
+def base():
+    return render_template('base.html')
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
