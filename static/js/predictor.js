b884fda1139b0e6bcfe2fc9d6019c2c94d7eb96c
Jack Cohen <jc@enpira.io>
2021-11-23 21:11:21 -0500

    Front end edits

--- static/js/predictor.js
+++ static/js/predictor.js
@@ -88,3 +88,28 @@ unique.forEach( (element) => {
     dropdown.add(opt);
 }
 );
+
+
+
+d3.selectAll("button").on("click", function () {
+    
+    var store = d3.select("#selStore").property("value");
+    var product = d3.select("#selProduct").property("value");
+    var week = d3.select("#selWeek").property("value");
+    var holiday = d3.select("#selHoliday").property("value");
+    var basePrice = d3.select("#basePrice").property("value");
+    var price = d3.select("#price").property("value");
+
+    fetch('/predictor', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(d => {
+        console.log('response', d)
+        var cost = d.cost
+
+        document.getElementById("output").innerHTML = "Annual Cost: $" + cost.toFixed(0);
